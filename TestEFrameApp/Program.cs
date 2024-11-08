fbe72aeabf9bebd753c352b49e37a51ff0e1a5d7
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-08 10:19:51 +0100

    Populating database for test

--- TestEFrameApp/Program.cs
+++ TestEFrameApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using PublishData1;
+using PublisherDomail;
 
 using (PubContext context = new PubContext())
 {
@@ -7,6 +9,31 @@ using (PubContext context = new PubContext())
 }
 
 GetAuthors();
+AddAuthor();
+GetAuthors();
+
+
+void AddAuthor()
+{
+    var addAuthor = new List<Author>
+    {  
+        new Author { FirstName = "Gabriel", LastName = "Lekane" },
+        new Author { FirstName = "Jos", LastName = "Mile" },
+        new Author { FirstName = "Goli", LastName = "Tafino" },
+        new Author { FirstName = "Gibri", LastName = "raphaele" },
+        new Author { FirstName = "jors", LastName = "bony" },
+        new Author { FirstName = "gio", LastName = "tabou" },
+        new Author { FirstName = "jordane", LastName = "Tsamo" },
+        new Author { FirstName = "Mr.Nafack", LastName = "Etienne" },
+    };
+
+    using var context = new PubContext();
+
+    context.Authors.AddRange(addAuthor);
+
+    context.SaveChanges();
+
+}
 void GetAuthors()
 {
     using var context = new PubContext();
