86561de4032047e3d54d4c4603af9f5616a7e845
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-08 10:44:17 +0100

    code refactoring

--- TestEFrameApp/Program.cs
+++ TestEFrameApp/Program.cs
@@ -9,42 +9,42 @@ using (PubContext context = new PubContext())
     context.Database.EnsureCreated();
 }
 
-//GetAuthors();
-//AddAuthor();
-//GetAuthors();
+GetAuthors();
+AddAuthor();
+GetAuthors();
 
-//void AddAuthor()
-//{
-//    var addAuthor = new List<Author>
-//    {  
-//        new Author { FirstName = "Gabriel", LastName = "Lekane" },
-//        new Author { FirstName = "Jos", LastName = "Mile" },
-//        new Author { FirstName = "Goli", LastName = "Tafino" },
-//        new Author { FirstName = "Gibri", LastName = "raphaele" },
-//        new Author { FirstName = "jors", LastName = "bony" },
-//        new Author { FirstName = "gio", LastName = "tabou" },
-//        new Author { FirstName = "jordane", LastName = "Tsamo" },
-//        new Author { FirstName = "Mr.Nafack", LastName = "Etienne" },
-//    };
+void AddAuthor()
+{
+    var addAuthor = new List<Author>
+    {
+        new Author { FirstName = "Gabriel", LastName = "Lekane" },
+        new Author { FirstName = "Jos", LastName = "Mile" },
+        new Author { FirstName = "Goli", LastName = "Tafino" },
+        new Author { FirstName = "Gibri", LastName = "raphaele" },
+        new Author { FirstName = "jors", LastName = "bony" },
+        new Author { FirstName = "gio", LastName = "tabou" },
+        new Author { FirstName = "jordane", LastName = "Tsamo" },
+        new Author { FirstName = "Mr.Nafack", LastName = "Etienne" },
+    };
 
-//    using var context = new PubContext();
+    using var context = new PubContext();
 
-//    context.Authors.AddRange(addAuthor);
+    context.Authors.AddRange(addAuthor);
 
-//    context.SaveChanges();
+    context.SaveChanges();
 
-//}
-//void GetAuthors()
-//{
-//    using var context = new PubContext();
+}
+void GetAuthors()
+{
+    using var context = new PubContext();
 
-//    var authors = context.Authors.ToList();
+    var authors = context.Authors.ToList();
 
-//    foreach (var auth in authors)
-//    {
-//        Console.WriteLine(auth.FirstName +" "+ auth.LastName);
-//    }
-//}
+    foreach (var auth in authors)
+    {
+        Console.WriteLine(auth.FirstName + " " + auth.LastName);
+    }
+}
 
 AddAuthorWithBook();
 GetAuthorsWithBooks();
