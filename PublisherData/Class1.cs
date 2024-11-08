76763ee14e455b113b63f8f624e0dda60c926b06
LekaneGabriel <lekanegabriel199@gmail.com>
2024-11-08 09:25:51 +0100

    Ajoutez des fichiers projet.

---
+++ PublisherData/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PublisherDomail;
+
+namespace PublishData1
+{
+    public class pubContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                    "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PublishData1");
+        }
+
+    }
+}
